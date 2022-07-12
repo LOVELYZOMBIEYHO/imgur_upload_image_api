5e86ede1729e2fcacbc2caf948b1ed456876dafb
LOVELYZOMBIEYHO <yimwingho2003@yahoo.com.hk>
2022-07-12 11:39:06 -0400

    first commit

---
+++ js/upload.js
@@ -0,0 +1,36 @@
+function reportInfo(vars, showType = false) {
+  if (showType === true) console.log(typeof vars);
+  console.log(vars);
+}
+
+function addImg(ele, content) {
+  var myDIV = document.querySelector(ele);
+  var newContent = document.createElement("div");
+  newContent.innerHTML = content;
+
+  while (newContent.firstChild) {
+    myDIV.appendChild(newContent.firstChild);
+  }
+}
+
+var feedback = function (res) {
+  reportInfo(res, true);
+  if (res.success === true) {
+    var get_link = res.data.link.replace(/^http:\/\//i, "https://");
+    document.querySelector(".status").classList.add("bg-success");
+    var content =
+      "Image : " +
+      '<br><input class="image-url" value="' +
+      get_link +
+      '"/>' +
+      '<img class="img" alt="Imgur-Upload" src="' +
+      get_link +
+      '"/>';
+    addImg(".status", content);
+  }
+};
+// for jseedav.com
+new Imgur({
+  clientid: "82592c12c4ed121", //You can change this ClientID
+  callback: feedback,
+});
